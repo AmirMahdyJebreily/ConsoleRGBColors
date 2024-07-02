5e068c4ff2b3bfb5df3ab5f59d8a3d0b0f5baccf
Amir JCS <amirmahdyjdx@gmail.com>
2024-07-03 01:04:41 +0330

    refactor: restructure repo

---
+++ tests/ConsoleRGBColors.UnitTests/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleRGBColors;
+using System.Drawing;
+
+RGBConsole.SetForeColor(Color.Bisque); // Use css colors in console application
+string repoLink = "https://github.com/AmirMahdyJebreilyhttps://github.com/AmirMahdyJebreily";
+Console.WriteLine($"Code agha github : {repoLink.ChangeStringForeColor(Color.Aqua)}"); // Change a string color
