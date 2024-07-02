de39d9c748d998b699fa4380190438de0bf8b6d6
Amir JCS <amirmahdyjdx@gmail.com>
2024-07-03 01:05:34 +0330

    test: add tests

--- tests/ConsoleRGBColors.UnitTests/Program.cs
+++ tests/ConsoleRGBColors.UnitTests/Program.cs
@@ -2,5 +2,5 @@
 using System.Drawing;
 
 RGBConsole.SetForeColor(Color.Bisque); // Use css colors in console application
-string repoLink = "https://github.com/AmirMahdyJebreilyhttps://github.com/AmirMahdyJebreily";
-Console.WriteLine($"Code agha github : {repoLink.ChangeStringForeColor(Color.Aqua)}"); // Change a string color
+string repoLink = "https://github.com/AmirMahdyJebreilyhttps://github.com/AmirMahdyJebreily".ChangeStringForeColor(Color.Aqua); // Change a string color
+Console.WriteLine($"Code agha github : {repoLink}");
