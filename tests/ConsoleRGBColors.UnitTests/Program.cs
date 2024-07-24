2c763b871ac686df3c1aed3f9c1f5608cedfd0f8
Amir JCS <amirmahdyjdx@gmail.com>
2024-07-24 17:46:14 +0330

    test: make test code better

--- tests/ConsoleRGBColors.UnitTests/Program.cs
+++ tests/ConsoleRGBColors.UnitTests/Program.cs
@@ -13,6 +13,35 @@ int r = 255,
     additiveIndex = 1, // 0, 1, 2
     decreaseIndex = 0; // 0, 1, 2
 int[] slopes = new int[3];
+
+Console.WriteLine("Codeagha Console RGB Color Test");
+Console.WriteLine("Github: https://github.com/AmirMahdyJebreily");
+
+for (int i = 1; i < 7; i++)
+{
+    if (i % 2 != 0)
+    {
+        slopes = new int[3];
+        slopes[additiveIndex] = 15;
+        additiveIndex = shiftIndexRight(additiveIndex, 2);
+    }
+    else
+    {
+        slopes = new int[3];
+        slopes[decreaseIndex] = -15;
+        decreaseIndex = shiftIndexRight(decreaseIndex, 2);
+    }
+    for (int j = 0; j < 17; j++)
+    {
+        r += slopes[0];
+        g += slopes[1];
+        b += slopes[2];
+        Console.Write("#".ChangeStringForeColor(r, g, b).ChangeStringBackColor(r, g, b));
+    }
+}
+Console.ResetColor();
+Console.WriteLine("\nFor Continue Press any button...");
+Console.ReadKey();
 for (int i = 1; i < 7; i++)
 {
     if (i % 2 != 0)
@@ -32,7 +61,9 @@ for (int i = 1; i < 7; i++)
         r += slopes[0];
         g += slopes[1];
         b += slopes[2];
-        Console.WriteLine("CODEAGHA".ChangeStringForeColor(r, g, b));
+        Console.Write(".::╟▒█▒┤CODEAGHA├▒█▒╢::.".ChangeStringForeColor(r, g, b));
+        Console.Write($"  ({r.ToString().ChangeStringForeColor(255, 0, 71)}, {g.ToString().ChangeStringForeColor(0, 255, 71)}, {b.ToString().ChangeStringForeColor(0, 71, 255)})");
+        Console.WriteLine("\t".ChangeStringForeColor(r, g, b));
+        await Task.Delay(1);
     }
 }
-
